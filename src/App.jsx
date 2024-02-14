7a9d6054a97aa579ecdef6b5202ec7bebcbf88ab
fayisvtp <fayizvtp@gmail.com>
2024-02-14 13:27:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import ImageUpload from './Component/ImageUpload'
+import Navbar from './Component/Navbar'
+import Desert from './Component/Desert'
+
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <ImageUpload/>
+    <Desert/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/ImageUpload.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import girl from '../assets/girl.png'
+import insta from '../assets/insta icon.png'
+export default function ImageUpload() {
+  return (
+    <>
+      <div className="col-12 bg-light shadow d-flex m-5 ">
+        <div className='instabg '>
+            <img src={insta} alt="rectange icon " />
+        </div>
+        <div className="buttnon d-flex  align-items-center justify-content-center  col-9" >
+  
+          <button style={{backgroundColor:'#C08C5D', border:'none', width:'60vh', height:'5vh',borderRadius:'30px',color:'white'}}>
+          Upload Images
+          </button>
+        </div>
+        <div className=''>
+            <img src= {girl} alt="girl" />
+        </div>
+      </div>
+    </>
+  )
+}
