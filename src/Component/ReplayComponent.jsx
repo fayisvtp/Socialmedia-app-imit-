6cee3fb6702b82144539c61fb19e69957ce17c4c
fayisvtp <fayizvtp@gmail.com>
2024-02-16 12:03:02 +0530

    final

--- src/Component/ReplayComponent.jsx
+++ src/Component/ReplayComponent.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ReplayComponent() {
+  const [replay, setreplay] = useState("");
+  const [replays, setreplays] = useState([]);
+
+  const addreplay = () => {
+    setreplays([...replays, { list: replay, id: Date.now() }]);
+    setreplay("");
+    console.log(replay);
+  };
+  
+
   return (
     <div>
        <div className="chat chat-end">
        <div className="chat-bubble bg-red-200 ">
-        replay man
+       {replays.map((values) => (
+          <li key={values.id}>
+            {values.list}
+            
+          </li>
+        ))}
        </div>
 
        </div>
+       <input
+            style={{ backgroundColor: "rgba(236, 200, 174, 0.5)" }}
+            className="h-14 col-9  text-black m-14 border-none rounded-box input-comment "
+            type="text"
+            placeholder="add replay"
+            value={replay}
+            onChange={(event) => setreplay(event.target.value)}
+            required
+          />
 
+          <button
+            className="relative right-0 top-0 bottom-0 m-auto"
+            onClick={addreplay}
+          >
+            Add
+          </button>
+      
     </div>
   )
 }
